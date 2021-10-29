b47cba7894d4e1918d8b1a0068b85c53f33279c9
Leonardo Pintor <jpintormore@uniminuto.edu.co>
2021-10-29 14:17:05 -0500

    se configura path de views

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000
 const rutas = require('./routers/main');
+app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.use('/',rutas)
 app.use('/about',rutas)
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,23 +1,12 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-app.use(express.static(path.resolve(__dirname, "./public")));
+const path = require('path')
 
 const mainController = {
     home: (req, res) => {
-        res.sendFile(__dirname + "/views/home.html")
+        res.sendFile(path.resolve(__dirname, '../views/home.html'))
     },
     about: (req, res) => {
-        res.sendFile(__dirname + "/views/about.html")
+        res.sendFile(path.resolve(__dirname, '../views/about.html'))
     }
-};
-// const mainController= {
-//     home: (req, res) => {
-//         res.send('HOLA')
-//     },
-//     about: (req, res) => {
-//         res.send('ABOUT')
-//     }
-// };
+}
 
 module.exports = mainController;
